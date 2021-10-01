55dcb5b4e0a49134c0bc8eda235fde8a8cec3c4d
wancat <wancat@wancat.cc>
2021-10-01 12:40:23 +0800

    Add acceleration lines

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,6 +1,7 @@
 let ball = {}
 const m = 2500
 const length = 20
+const aLength = 100
 const SPACE = 32
 
 function setup() {
@@ -34,17 +35,33 @@ function drawBall() {
   if (mouseIsPressed) {
     stroke(0)
     line(ball.x, ball.y, mouseX, mouseY)
-    text(`${resultantForce(ball.a[0], ball.a[1])}`, mouseX, mouseY)
+    text(`${pretty(hypotenuse(ball.a[0], ball.a[1]))}`, mouseX, mouseY)
+    stroke(100, 100, 255)
+    strokeWeight(4)
+    drawLine(tangent(ball.a, ball.v), aLength)
+    stroke(100, 200, 100)
+    drawLine(normalA(ball.a, ball.v), aLength)
   }
-  vp = [ball.x + ball.v[0] * length, ball.y + ball.v[1] * length]
   stroke(200, 100, 100)
-  line(ball.x, ball.y, vp[0], vp[1])
+  strokeWeight(1)
+  drawLine(ball.v, length)
   stroke(0)
   ellipse(ball.x, ball.y, ball.r * 2 , ball.r * 2)
 }
 
-function resultantForce(a, b) {
-  return Math.round(Math.sqrt(a*a + b*b) * 100) / 100
+function drawLine(vector, length) {
+  vp = [ball.x + vector[0] * length, ball.y + vector[1] * length]
+  textSize(24)
+  text(`${pretty(hypotenuse(vector[0], vector[1]))}`, vp[0], vp[1])
+  line(ball.x, ball.y, vp[0], vp[1])
+}
+
+function hypotenuse(a, b) {
+  return Math.sqrt(a*a + b*b)
+}
+
+function pretty(x, n = 100) {
+  return Math.round(x * n) / n
 }
 
 function updateBall() {
@@ -63,4 +80,15 @@ function keyPressed() {
   if (keyCode === SPACE) {
     ball.v = [0, 0]
   }
+}
+
+function tangent(a, v) {
+  const at = a[0] * v[0] + a[1] * v[1] // dot
+  const l = hypotenuse(v[0], v[1])
+  return [at * v[0] / l, at * v[1] / l]
+}
+
+function normalA(a, v) {
+  const uv = [v[1], -v[0]]
+  return tangent(a, uv)
 }
