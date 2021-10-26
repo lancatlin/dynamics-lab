97f57c3e192efb0b04d5d0c0dfd1313848a61cdf
wancat <wancat@wancat.cc>
2021-10-26 23:07:20 +0800

    Rewrite the code by p5.Vector

--- acceleration/sketch.js
+++ acceleration/sketch.js
@@ -1,31 +1,32 @@
 let ball = {}
 const m = 2500
 const length = 20
-const aLength = 100
+const aLength = 200
 const SPACE = 32
+const usage = "Left click to pull the ball.\nSpace to stop the ball."
 
 function setup() {
-  // put setup code here
   createCanvas(windowWidth, windowHeight)
   ball = {
-    x: windowWidth / 2,
-    y: windowHeight / 2,
+    p: createVector(windowWidth / 2, windowHeight / 2),
     r: 15,
-    v: [5, 5],
-    a: [0, -0.2],
+    v: createVector(5, 5),
+    a: createVector(0, 0),
   }
 }
 
 function draw() {
-  // put drawing code here
   background(220)
+  textSize(16)
+  strokeWeight(1)
+  text(usage, 20, 30)
   if (mouseIsPressed) {
-    ball.a = [
-      (mouseX - ball.x) / m,
-      (mouseY - ball.y) / m,
-    ]
+    ball.a.set(
+      (mouseX - ball.p.x) / m,
+      (mouseY - ball.p.y) / m,
+    )
   } else {
-    ball.a = [0, 0]
+    ball.a.set(0, 0)
   }
   updateBall()
   drawBall()
@@ -34,30 +35,26 @@ function draw() {
 function drawBall() {
   if (mouseIsPressed) {
     stroke(0)
-    line(ball.x, ball.y, mouseX, mouseY)
-    text(`${pretty(hypotenuse(ball.a[0], ball.a[1]))}`, mouseX, mouseY)
-    stroke(100, 100, 255)
-    strokeWeight(4)
-    drawLine(tangent(ball.a, ball.v), aLength)
-    stroke(100, 200, 100)
-    drawLine(normalA(ball.a, ball.v), aLength)
+    line(ball.p.x, ball.p.y, mouseX, mouseY)
+    text(`${pretty(ball.a.mag())}`, mouseX, mouseY)
+    // stroke(100, 100, 255)
+    // strokeWeight(4)
+    // drawLine(tangent(ball.a, ball.v), aLength)
+    // stroke(100, 200, 100)
+    // drawLine(normalA(ball.a, ball.v), aLength)
   }
   stroke(200, 100, 100)
   strokeWeight(1)
   drawLine(ball.v, length)
   stroke(0)
-  ellipse(ball.x, ball.y, ball.r * 2 , ball.r * 2)
+  ellipse(ball.p.x, ball.p.y, ball.r * 2 , ball.r * 2)
 }
 
 function drawLine(vector, length) {
-  vp = [ball.x + vector[0] * length, ball.y + vector[1] * length]
+  const vp = p5.Vector.add(ball.p, p5.Vector.mult(vector, length))
   textSize(24)
-  text(`${pretty(hypotenuse(vector[0], vector[1]))}`, vp[0], vp[1])
-  line(ball.x, ball.y, vp[0], vp[1])
-}
-
-function hypotenuse(a, b) {
-  return Math.sqrt(a*a + b*b)
+  text(`${pretty(vector.mag())}`, vp.x, vp.y)
+  line(ball.p.x, ball.p.y, vp.x, vp.y)
 }
 
 function pretty(x, n = 100) {
@@ -65,14 +62,13 @@ function pretty(x, n = 100) {
 }
 
 function updateBall() {
-  ball.v = ball.v.map((v, i) => v + ball.a[i])
-  ball.x += ball.v[0]
-  ball.y += ball.v[1]
-  if (ball.x > windowWidth || ball.x < 0) {
-    ball.v[0] *= -1
+  ball.v.add(ball.a)
+  ball.p.add(ball.v)
+  if (ball.p.x > windowWidth - ball.r || ball.p.x < ball.r) {
+    ball.v.x *= -1
   }
-  if (ball.y > windowHeight || ball.y < 0) {
-    ball.v[1] *= -1
+  if (ball.p.y > windowHeight - ball.r || ball.p.y < ball.r) {
+    ball.v.y *= -1
   }
 }
 
@@ -83,12 +79,18 @@ function keyPressed() {
 }
 
 function tangent(a, v) {
-  const at = a[0] * v[0] + a[1] * v[1] // dot
-  const l = hypotenuse(v[0], v[1])
-  return [at * v[0] / l, at * v[1] / l]
+  const l = hypotenuse(v)
+  const at = (a[0] * v[0] + a[1] * v[1]) / l // dot
+  return v.map(x => x * at / l)
+}
+
+function squareSum(v) {
+  return v[0] * v[0] + v[1] * v[1]
 }
 
 function normalA(a, v) {
-  const uv = [v[1], -v[0]]
-  return tangent(a, uv)
+  const vl = hypotenuse(v)
+  const uv = [ v[1], -v[0] ]
+  const an = Math.sqrt(squareSum(a), squareSum(tangent(a, v)))
+  return uv.map(x => x * an / vl)
 }
