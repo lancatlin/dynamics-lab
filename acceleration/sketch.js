4c22ffdf59aab7bb5023396e6b157134f2320e62
wancat <wancat@wancat.cc>
2021-10-29 11:01:39 +0800

    Split ball from acceleration

--- acceleration/sketch.js
+++ acceleration/sketch.js
@@ -1,97 +1,53 @@
-let ball = {}
-const m = 1
-const length = 1
-const aLength = length
-const SPACE = 32
-const usage = "Left click to pull the ball.\nSpace to stop the ball.\n"
+let ball = null;
+const m = 1;
+const length = 1;
+const aLength = length;
+const SPACE = 32;
+const usage = "Left click to pull the ball.\nSpace to stop the ball.\n";
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  ball = {
-    p: createVector(windowWidth / 2, windowHeight / 2),
-    r: 15,
-    v: createVector(5, 5),
-    a: createVector(0, 0),
-  }
+  createCanvas(windowWidth, windowHeight);
+  ball = new Ball(windowWidth / 2, windowHeight / 2, 15);
 }
 
 function draw() {
-  background(220)
-  textSize(16)
-  strokeWeight(1)
-  text(usage+`fps: ${Math.round(frameRate())}`, 20, 30)
-  updateBall()
-  drawBall()
+  background(220);
+  textSize(16);
+  strokeWeight(1);
+  text(usage + `fps: ${Math.round(frameRate())}`, 20, 30);
+  ball.update();
+  if (mouseIsPressed) {
+    ball.a.set((mouseX - ball.p.x) / m, (mouseY - ball.p.y) / m);
+  } else {
+    ball.a.set(0, 0);
+  }
+  drawBall();
 }
 
 function drawBall() {
   if (mouseIsPressed) {
-    stroke(0)
+    stroke(0);
     // line(ball.p.x, ball.p.y, mouseX, mouseY)
-    drawLine(ball.a, aLength)
-    stroke(100, 100, 255)
-    strokeWeight(4)
-    const t = tangent(ball.a, ball.v)
-    drawLine(t, aLength)
-    stroke(100, 200, 100)
-    const n = normalA(ball.a, ball.v)
-    drawLine(n, aLength)
+    ball.drawLine(ball.a, aLength);
+    stroke(100, 100, 255);
+    strokeWeight(4);
+    ball.drawLine(ball.tangentAcceleration(), length);
+    stroke(100, 200, 100);
+    ball.drawLine(ball.normalAcceleration(), length);
   }
-  stroke(200, 100, 100)
-  strokeWeight(1)
-  drawLine(ball.v, length)
-  stroke(0)
-  ellipse(ball.p.x, ball.p.y, ball.r * 2 , ball.r * 2)
-}
-
-function drawLine(vector, length) {
-  const vp = p5.Vector.add(ball.p, p5.Vector.mult(vector, length))
-  textSize(24)
-  text(`${pretty(vector.mag())}`, vp.x, vp.y)
-  line(ball.p.x, ball.p.y, vp.x, vp.y)
+  stroke(200, 100, 100);
+  strokeWeight(1);
+  ball.drawLine(ball.v, length);
+  stroke(0);
+  ball.draw();
 }
 
 function pretty(x, n = 1) {
-  return Math.round(x * n) / n
-}
-
-function updateBall() {
-  ball.v.add(p5.Vector.div(ball.a, frameRate()))
-  ball.p.add(p5.Vector.div(ball.v, frameRate()))
-  if (ball.p.x > windowWidth - ball.r) {
-    ball.v.x = -Math.abs(ball.v.x)
-  }
-  if (ball.p.x < ball.r) {
-    ball.v.x = Math.abs(ball.v.x)
-  }
-  if (ball.p.y > windowHeight - ball.r) {
-    ball.v.y = -Math.abs(ball.v.y)
-  }
-  if(ball.p.y < ball.r) {
-    ball.v.y = Math.abs(ball.v.y)
-  }
-  if (mouseIsPressed) {
-    ball.a.set(
-      (mouseX - ball.p.x) / m,
-      (mouseY - ball.p.y) / m,
-    )
-  } else {
-    ball.a.set(0, 0)
-  }
+  return Math.round(x * n) / n;
 }
 
 function keyPressed() {
   if (keyCode === SPACE) {
-    ball.v.set(0, 0)
+    this.v.set(0, 0);
   }
-}
-
-function tangent(a, v) {
-  const ut = p5.Vector.normalize(v)
-  return p5.Vector.mult(ut, a.dot(ut))
-}
-
-function normalA(a, v) {
-  const un = p5.Vector.normalize(p5.Vector.rotate(v, HALF_PI))
-  return p5.Vector.mult(un, a.dot(un))
 }
---
+++ ball.js
@@ -0,0 +1,54 @@
+class Ball {
+  constructor(x, y, r = 15) {
+    this.p = createVector(x, y);
+    this.v = createVector(0, 0);
+    this.a = createVector(0, 0);
+    this.r = r;
+  }
+
+  get x() {
+    return this.p.x;
+  }
+
+  get y() {
+    return this.p.y;
+  }
+
+  update() {
+    this.v.add(p5.Vector.div(this.a, frameRate()));
+    this.p.add(p5.Vector.div(this.v, frameRate()));
+    if (this.p.x > windowWidth - this.r) {
+      this.v.x = -Math.abs(this.v.x);
+    }
+    if (this.p.x < this.r) {
+      this.v.x = Math.abs(this.v.x);
+    }
+    if (this.p.y > windowHeight - this.r) {
+      this.v.y = -Math.abs(this.v.y);
+    }
+    if (this.p.y < this.r) {
+      this.v.y = Math.abs(this.v.y);
+    }
+  }
+
+  tangentAcceleration() {
+    const ut = p5.Vector.normalize(this.v);
+    return p5.Vector.mult(ut, this.a.dot(ut));
+  }
+
+  normalAcceleration() {
+    const un = p5.Vector.normalize(p5.Vector.rotate(this.v, HALF_PI));
+    return p5.Vector.mult(un, this.a.dot(un));
+  }
+
+  drawLine(vector, length) {
+    const vp = p5.Vector.add(this.p, p5.Vector.mult(vector, length));
+    textSize(24);
+    text(`${pretty(vector.mag())}`, vp.x, vp.y);
+    line(this.x, this.y, vp.x, vp.y);
+  }
+
+  draw() {
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+}
