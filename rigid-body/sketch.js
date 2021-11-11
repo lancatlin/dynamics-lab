af67ef82794bbde4a0f248d8f7a502a2e2b2f857
wancat <wancat@wancat.cc>
2021-11-11 14:47:59 +0800

    Add pin point

--- rigid-body/sketch.js
+++ rigid-body/sketch.js
@@ -12,18 +12,26 @@ function draw() {
 
 class Rect {
   constructor() {
-    this.p = createVector(windowWidth / 2, windowHeight / 2);
-    this.v = createVector(30, 30);
+    this.p = createVector(300, 400);
+    this.v = createVector(0, 0);
     this.shape = createVector(50, 100);
-    this.theta = createVector(0, 0, PI / 3);
-    this.omega = createVector(0, 0, 5);
+    this.theta = createVector(0, 0, PI / 12);
+    this.omega = createVector(0, 0, 1);
+    this.pin = createVector(10, -10).add(this.p);
   }
 
   draw() {
+    strokeWeight(1);
     rectMode(CENTER);
-    translate(this.p.x, this.p.y);
+    translate(this.p);
     rotate(this.theta.z);
     rect(0, 0, this.shape.x, this.shape.y);
+
+    resetMatrix();
+    rotate(0);
+    translate(0, 0);
+    strokeWeight(10);
+    point(this.pin);
   }
 
   update() {
