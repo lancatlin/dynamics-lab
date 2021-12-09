750157ae3b66fca20250bb5cc777c483bb7feff0
wancat <wancat@wancat.cc>
2021-12-09 12:54:31 +0800

    Change to rect

--- rigid-body/sketch.js
+++ rigid-body/sketch.js
@@ -22,7 +22,7 @@ class Rect {
     this.v = createVector(0, 0);
     this.theta = createVector(0, 0, PI / 12);
     this.omega = createVector(0, 0, 0);
-    this.pin = createVector(100, -30).add(this.p);
+    this.pin = createVector(100, -50).add(this.p);
     this.r = createVector(0, 0);
   }
 
@@ -30,9 +30,9 @@ class Rect {
     strokeWeight(1);
     rectMode(CENTER);
     translate(this.p);
-    // rotate(this.theta.z);
-    // rect(0, 0, this.shape.x, this.shape.y);
-    ellipse(0, 0, 30, 30);
+    rotate(this.theta.z);
+    rect(0, 0, this.shape.x, this.shape.y);
+    // ellipse(0, 0, 30, 30);
 
     resetMatrix();
     rotate(0);
@@ -49,14 +49,14 @@ class Rect {
     // const a = p5.Vector.sub(Ft, F);
     const r = p5.Vector.sub(this.pin, this.p);
     const r_dot = p5.Vector.sub(r, this.r);
-    const I = this.mass * r.magSq();
+    const I = this.inertia + this.mass * r.magSq();
     const F = p5.Vector.mult(g, this.mass); // F=ma
     const alpha = r.cross(F).div(I);
     // const a = alpha.cross(r).add(p5.Vector.mult(r_dot, this.omega).mult(2));
 
     // console.log(r);
     this.omega.add(p5.Vector.div(alpha, frameRate()));
-    this.theta.add(p5.Vector.div(this.omega, frameRate()));
+    this.theta.add(p5.Vector.div(this.omega, -frameRate()));
     this.v.set(this.omega.cross(r));
     this.p.add(p5.Vector.div(this.v, frameRate()));
   }
